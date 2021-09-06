eadb1be647269623e22100cb5bd1f138600eaac0
amandine carreno <30933638+carra1358@users.noreply.github.com>
2021-09-06 13:39:24 +0200

    search with tag
    -les tags fonctionnes individuellement

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const advanceSearchByIngredients = document.getElementById("search_ingredients")
 const advanceSearchByAppliance = document.getElementById("search_appliances");
 const advanceSearchByUstensils = document.getElementById("search_ustensils");
 const selectIngredients = document.getElementById("select_ingredients");
-const selectUstensils = document.getElementById("search_ustensils");
-const searchIngredients = document.getElementById("search_ingredients");
+const selectUstensils = document.getElementById("select_ustensils");
+const selectAppareil = document.getElementById("select_appareil");
 const loadIngredients = document.getElementById("load_ingredients");
+const loadAppliances = document.getElementById("load_appliances");
+const loadUstensils = document.getElementById("load_ustensils");
 const tags = document.getElementById("tags");
 
 // START CODE
@@ -29,6 +31,8 @@ class Filter {
         this.recipes = data;
         this.ingredients = [];
         this.selectedIngredients = [];
+        this.selectedAppliances = [];
+        this.selectedUstensils = [];
         this.appliances = [];
         this.ustensils =[];
         this.query = null;
@@ -52,7 +56,14 @@ filterWithSearchBar(query){
                 this.ingredients = [...i];
 
                this.appliances = this.result.map(r => r.appliance);
+               this.appliances = new Set([...this.appliances]);
+               this.appliances = [...this.appliances];
+               
                this.ustensils = this.result.map(u => u.ustensils);
+               this.ustensils = this.ustensils.reduce(function(a,b){ return [...a,...b]});
+               this.ustensils = new Set([...this.ustensils]);
+               this.ustensils = [...this.ustensils];
+               console.log(this.ustensils)
            }
             
        }
@@ -61,13 +72,137 @@ filterWithAdvancesdSearchBar(arr,requete){
      return arr.filter(el =>  el.toLowerCase().indexOf(requete.toLowerCase()) !== -1);
 }
 
-addOrRemove(tagName){
+addOrRemoveUstensils(){}
+
+addOrRemoveAppliance(tagName){
+console.log(this.result)
+console.log(tagName)
+if(this.selectedAppliances.includes(tagName)){
+    this.selectedAppliances = this.selectedAppliances.filter(t => t !== tagName);
+    this.appliances.push(tagName);
+    this.filterWithSearchBar(this.query)
+     
+    if(!this.selectedAppliances.length == 0){
+        this.result = this.result.filter(r => {
+            const result = this.selectedAppliances[this.selectedAppliances.lengths -1].includes(r.appliance).toLowerCase()
+        
+            return result
+        }) 
+        
+    }
+    
+}else{
+    this.selectedAppliances.push(tagName);
+    console.log(this.result)
+    this.result = this.result.filter(r => {
+
+     console.log(r.appliance, this.selectedAppliances);
+    const t = r.appliance.toLowerCase().includes(this.selectedAppliances[this.selectedAppliances.length - 1].toLowerCase());
+    console.log(t)
+    return t
+    
+    }) 
+}
+this.appliances = this.result.map(r => r.appliance);
+this.appliances = this.appliances.filter(x => !this.selectedAppliances.includes(x))
+  
+
+}
+/*
+addOrRemoveUstensils(tagName){
+    console.log(this.result)
+    console.log(tagName)
+    if(this.selectedUstensils.includes(tagName)){
+        this.selectedUstensils = this.selectedUstensils.filter(t => t !== tagName);
+        this.ustensils.push(tagName);
+        this.filterWithSearchBar(this.query)
+         
+        if(!this.selectedUstensils.length == 0){
+            this.result = this.result.filter(r => {
+
+                console.log(r.ustensils, this.selectedUstensils);
+               const tu = r.ustensils.toLowerCase().includes(this.selectedUstensils[this.selectedUstensils.length - 1].toLowerCase());
+               console.log(tu)
+               return tu
+               
+               }) 
+            
+        }
+        
+    }else{
+        this.selectedUstensils.push(tagName);
+        console.log(this.result)
+        this.result = this.result.filter(r => {
+
+            console.log(r.ustensils, this.selectedUstensils);
+           const tu = r.ustensils.includes(this.selectedUstensils[this.selectedUstensils.length - 1].toLowerCase());
+           console.log(tu)
+           return tu
+           
+           }) 
+    }
+    this.ustensils = this.result.map(r => r.ustensils);
+    this.ustensils = this.ustensils.reduce(function(a,b){ return [...a,...b]});
+    this.ustensils = new Set([...this.ustensils]);
+    this.ustensils = [...this.ustensils]
+    this.ustensils= this.ustensils.filter(x => !this.selectedUstensils.includes(x))
+      
+    
+    }*/
+    addOrRemoveUstensils(tagName){
+        console.log(this.result)
+        console.log(tagName)
+        if(this.selectedUstensils.includes(tagName)){
+            this.selectedUstensils = this.selectedUstensils.filter(t => t !== tagName);
+            this.ustensils.push(tagName);
+            this.filterWithSearchBar(this.query)
+             
+            if(!this.selectedUstensils.length == 0){
+                this.result = this.result.filter(r => {
+    
+                    console.log(r.ustensils, this.selectedUstensils);
+                   const tu = r.ustensils.some(u => {
+                       console.log(u)
+                       return this.selectedUstensils[this.selectedUstensils.length - 1].includes(u)
+                   })
+                   console.log(tu)
+                   return tu
+                   
+                   }) 
+                
+            }
+            
+        }else{
+            this.selectedUstensils.push(tagName);
+            console.log(this.result)
+            this.result = this.result.filter(r => {
+    
+                console.log(r.ustensils, this.selectedUstensils);
+               const tu = r.ustensils.some(u => {
+                   console.log(u)
+                   return this.selectedUstensils[this.selectedUstensils.length - 1].includes(u)
+               })
+               console.log(tu)
+               return tu
+               
+               }) 
+        }
+        this.ustensils = this.result.map(r => r.ustensils);
+        this.ustensils = this.ustensils.reduce(function(a,b){ return [...a,...b]});
+        this.ustensils = new Set([...this.ustensils]);
+        this.ustensils = [...this.ustensils]
+        this.ustensils= this.ustensils.filter(x => !this.selectedUstensils.includes(x))
+          
+        
+        }
+
+addOrRemoveIngredient(tagName){
     
     if(this.selectedIngredients.includes(tagName)){
     this.selectedIngredients = this.selectedIngredients.filter(t => t !== tagName);
     this.ingredients.push(tagName);
     this.filterWithSearchBar(this.query);
-    console.log(this.result);
+
     if(!this.selectedIngredients.length == 0){
         this.result = this.result.filter(r => {
             const result = r.ingredients.some(i => 
@@ -109,9 +244,6 @@ addOrRemove(tagName){
 
     }
 
-  
-
-
 }
 
 
@@ -152,31 +284,117 @@ searchBar.addEventListener("input", e => {
 
 
     }},300)
+    // select ustensils tags
+    function openSelectUstensils(){
+        selectUstensils.addEventListener("click", () => {
+            advanceSearchByUstensils.removeAttribute("placeholder");
+            advanceSearchByUstensils.setAttribute("placeholder", "rechercher un ustensil");
+            loadUstensils.classList.replace("bi-chevron-down", "bi-chevron-up");
+            advanceSearchResultsUstensils.innerHTML = "";
+            advanceSearchResultsUstensils.innerHTML += `${filter.ustensils.map(r => `<li class="add_tag">${r}</li>`).join(" ")}`
+            advanceSearchResultsUstensils.querySelectorAll(".add_tag").forEach(el => {
+        
+                el.addEventListener("click", e => {      
+                const nameTag = e.target.textContent;
+                 filter.addOrRemoveUstensils(nameTag);
+                 advanceSearchResultsUstensils.innerHTML = "";
+                renderAll = recipeCardTemplate.innerHTML = "";
+                renderAll = filter.result.forEach(el => renderCards(el));
+                tags.innerHTML += `<li class="tag tag_u" data-tag="${nameTag}"><span>${nameTag}</span><i class="bi bi-x-circle" ></i></li>`;
+            tags.querySelectorAll(".tag_u").forEach(el => {
+             el.addEventListener("click", ()=> {
+                 const su = el.getAttribute("data-tag");
+                 console.log(su)
+                 filter.addOrRemoveUstensils(su);
+                 tags.removeChild(el);
+                 renderAll = recipeCardTemplate.innerHTML = "";
+                 renderAll = filter.result.forEach(el => renderCards(el));
+                 
+             })  
+           }) 
     
     
-   function openSelectIngredients () {
-    selectIngredients.addEventListener("click", () => {
-        searchIngredients.removeAttribute("placeholder");
-        searchIngredients.setAttribute("placeholder", "rechercher un ingrédient");
-        loadIngredients.classList.replace("bi-chevron-down", "bi-chevron-up");
-        advanceSearchResultsIngredients.innerHTML = "";
-        advanceSearchResultsIngredients.innerHTML += `${filter.ingredients.map(r => `<li class="add_tag">${r}</li>`).join("")}`
-        advanceSearchResultsIngredients.querySelectorAll(".add_tag").forEach(el => {
+    
+                })
+    
+         
+         })
+  
+    
+     
+
+})
+
+    }
+    
+    // select ingredients tags
+
+    function openSelectIngredients () {
+        selectIngredients.addEventListener("click", () => {
+            advanceSearchByIngredients.removeAttribute("placeholder");
+            advanceSearchByIngredients.setAttribute("placeholder", "rechercher un ingrédient");
+            loadIngredients.classList.replace("bi-chevron-down", "bi-chevron-up");
+            advanceSearchResultsIngredients.innerHTML = "";
+            advanceSearchResultsIngredients.innerHTML += `${filter.ingredients.map(r => `<li class="add_tag">${r}</li>`).join(" ")}`
+            advanceSearchResultsIngredients.querySelectorAll(".add_tag").forEach(el => {
+        
+                el.addEventListener("click", e => {      
+                const nameTag = e.target.textContent;
+                 filter.addOrRemoveIngredient(nameTag);
+                 advanceSearchResultsIngredients.innerHTML = "";
+                renderAll = recipeCardTemplate.innerHTML = "";
+                renderAll = filter.result.forEach(el => renderCards(el));
+                tags.innerHTML += `<li class="tag tag_i" data-tag="${nameTag}"><span>${nameTag}</span><i class="bi bi-x-circle" ></i></li>`;
+            tags.querySelectorAll(".tag_i").forEach(el => {
+             el.addEventListener("click", ()=> {
+                 const s = el.getAttribute("data-tag");
+                 console.log(s)
+                 filter.addOrRemoveIngredient(s);
+                 tags.removeChild(el);
+                 renderAll = recipeCardTemplate.innerHTML = "";
+                 renderAll = filter.result.forEach(el => renderCards(el));
+                 
+             })  
+           }) 
+    
+    
+    
+                })
+    
+         
+         })
+  
+    
+     
+
+})
+
+
+
+}
+// Select appareils tags 
+
+function openSelectAppliances () {
+    selectAppareil.addEventListener("click", () => {
+        advanceSearchByAppliance.removeAttribute("placeholder");
+        advanceSearchByAppliance.setAttribute("placeholder", "rechercher un appareil");
+        loadAppliances.classList.replace("bi-chevron-down", "bi-chevron-up");
+        advanceSearchResultsAppliance.innerHTML = "";
+        advanceSearchResultsAppliance.innerHTML += `${filter.appliances.map(r => `<li class="add_tag">${r}</li>`).join("")}`
+        advanceSearchResultsAppliance.querySelectorAll(".add_tag").forEach(el => {
     
             el.addEventListener("click", e => {      
             const nameTag = e.target.textContent;
-             filter.addOrRemove(nameTag);
-             advanceSearchResultsIngredients.innerHTML = "";
+             filter.addOrRemoveAppliance(nameTag);
+             advanceSearchResultsAppliance.innerHTML = "";
             renderAll = recipeCardTemplate.innerHTML = "";
             renderAll = filter.result.forEach(el => renderCards(el));
-            tags.innerHTML += `<li class="tag tag_i" data-tag="${nameTag}"><span>${nameTag}</span><i class="bi bi-x-circle" ></i></li>`;
-        tags.querySelectorAll(".tag_i").forEach(el => {
-         el.addEventListener("click", (e)=> {
-             const s = el.getAttribute("data-tag");
-             const rt = el;
-             console.log(rt)
-             console.log(s)
-             filter.addOrRemove(s);
+            tags.innerHTML += `<li class="tag tag_a" data-tag="${nameTag}"><span>${nameTag}</span><i class="bi bi-x-circle" ></i></li>`;
+        tags.querySelectorAll(".tag_a").forEach(el => {
+         el.addEventListener("click", ()=> {
+             const sa = el.getAttribute("data-tag");
+             console.log(sa)
+             filter.addOrRemoveAppliance(sa);
              tags.removeChild(el);
              renderAll = recipeCardTemplate.innerHTML = "";
              renderAll = filter.result.forEach(el => renderCards(el));
@@ -187,16 +405,25 @@ searchBar.addEventListener("input", e => {
 
 
             })
+
      
-        })
-      
+     })
+
+
+ 
 
 })
 
 
+
 }
 
+openSelectUstensils();
+
+openSelectAppliances();
+
 openSelectIngredients();
+
 
 
 
